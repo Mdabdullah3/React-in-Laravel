34ffe73dc38e072654e7981cefd2ef42096c9a11
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2024-01-11 21:06:19 +0600

    new changes

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -10,7 +10,21 @@ const Dashboard = () => {
     return (
         <DashboardLayout sidebarLinks={sidebarLinks}>
             <h1>Dashboard</h1>
-            <p>Your dashboard content goes here.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                necessitatibus et labore quae nam commodi animi rerum fugiat
+                dolore mollitia accusamus, enim illum aliquam, iure temporibus
+                laboriosam, ullam eligendi assumenda distinctio perspiciatis
+                asperiores tenetur vitae earum vero! Quam illo saepe eligendi
+                repudiandae? Quia repudiandae, inventore eligendi cumque itaque
+                tenetur incidunt necessitatibus molestiae, laudantium harum ipsa
+                aliquid laboriosam excepturi vel eos! Ullam voluptatibus ex,
+                dignissimos at sapiente ipsum harum explicabo quod magni,
+                temporibus tempore doloribus. Dolor, amet libero tempora quam
+                culpa harum provident et recusandae, saepe eaque facilis dolorem
+                veritatis sint in id possimus quasi. Sed ab incidunt ad sit
+                eaque?
+            </p>
         </DashboardLayout>
     );
 };
